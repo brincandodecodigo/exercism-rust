c7ce8172236d9f69d8a99ef86b6da375615101b4
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-01 19:42:19 -0300

    Raindrops

---
+++ raindrops/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn raindrops(n: u32) -> String {
+    let mut result: String = String::new();
+
+    if n % 3 == 0 {
+        result.push_str("Pling");
+    }
+
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    }
+
+    if n % 7 == 0 {
+        result.push_str("Plong");
+    }
+
+    if result.is_empty() {
+        result = n.to_string()
+    }
+
+    result
+}
